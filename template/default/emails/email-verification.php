8a8e8284136527c2aacce67a54a96c483a472edd
-- <-->
2019-09-20 06:59:30 +0100

    Update email-verification.php

--- template/default/emails/email-verification.php
+++ template/default/emails/email-verification.php
@@ -1,7 +1,7 @@
 <?php include 'includes/header.php';?>
 
 
-      <span style="text-transform: capitalize; color: #203864"><?=$user['fullname'];?></span>!
+      <span style="text-transform: capitalize; color: #203864">Hi <?=$user['fullname'];?>,</span>
       </td></tr></tbody></table>
 
 
